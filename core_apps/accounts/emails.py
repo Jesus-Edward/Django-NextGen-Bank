dddaf43778fe274cdc9e52fbd1016e04d3b4aee3
Thompson Edward <AugustJnr@AugustJnr.local>
2026-05-08 06:48:47 +0100

    feat: tellers can deposit money to account

--- core_apps/accounts/emails.py
+++ core_apps/accounts/emails.py
@@ -39,7 +39,6 @@ def send_account_activation_email(account: BankAccount):
     context = {
         "account": account,
         "site_name": settings.SITE_NAME,
-        "expiry_time": settings.OTP_EXPIRATION,
     }
 
     html_email = render_to_string("emails/bank_account_activated.html", context=context)
@@ -54,3 +53,30 @@ def send_account_activation_email(account: BankAccount):
         logger.info(f"Account activvation email sent to {account.user.email} successfully")
     except Exception as e:
         logger.error(f"Failed to send Account activation email to {account.user.email}: Error: {str(e)}")
+
+def send_deposit_email(user, user_email, amount, currency, new_balance, account_number):
+    subject = _("Deposit Confirmation")
+    from_email = settings.DEFAULT_FROM_EMAIL
+    recipient_email = [user_email]
+
+    context = {
+        "user": user,
+        "amount": amount,
+        "currency": currency,
+        "account_number": account_number,
+        "new_balance": new_balance,
+        "site_name": settings.SITE_NAME,
+    }
+
+    html_email = render_to_string("emails/deposit_confirmation.html", context=context)
+    plain_email = strip_tags(html_email)
+    email = EmailMultiAlternatives(
+        subject=subject, body=plain_email, from_email=from_email, to=recipient_email
+    )
+    email.attach_alternative(html_email, "text/html")
+
+    try:
+        email.send()
+        logger.info(f"Deposit Confirmation email sent to {user_email} successfully")
+    except Exception as e:
+        logger.error(f"Failed to send Deposit Confirmation email to {user_email}: Error: {str(e)}")
--- core_apps/accounts/views.py
+++ core_apps/accounts/views.py
@@ -1,14 +1,21 @@
 from typing import Any
 
-from django.utils import timezone 
+from django.utils import timezone
 from rest_framework.request import Request
 from rest_framework import generics, status
 from rest_framework.response import Response
-from core_apps.common.permissions import IsAccountExecutive
-from .emails import send_account_activation_email
+from core_apps.common.permissions import IsAccountExecutive, IsTeller
+from .emails import send_account_activation_email, send_deposit_email
 from core_apps.common.renderers import GenericJSONRenderer
 from .models import BankAccount
-from .serializers import AccountVerificationSerializer
+from .serializers import (
+    AccountVerificationSerializer,
+    DepositSerializer,
+    CustomerInfoSerializer,
+)
+from django.db import transaction
+from loguru import logger
+
 
 class AccountVerificationView(generics.UpdateAPIView):
     queryset = BankAccount.objects.all()
@@ -23,24 +30,24 @@ class AccountVerificationView(generics.UpdateAPIView):
 
         if instance.kyc_verified and instance.fully_activated:
             return Response(
-                {
-                    "message": "This Account is already activated and fully activated."
-                },
-                status=status.HTTP_400_BAD_REQUEST
+                {"message": "This Account is already activated and fully activated."},
+                status=status.HTTP_400_BAD_REQUEST,
             )
 
         partial = kwargs.pop("partial", False)
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         if serializer.is_valid(raise_exception=True):
-            kyc_submitted = serializer.validated_data.get("kyc_submitted", instance.kyc_submitted)
-            kyc_verified = serializer.validated_data.get("kyc_verified", instance.kyc_verified)
+            kyc_submitted = serializer.validated_data.get(
+                "kyc_submitted", instance.kyc_submitted
+            )
+            kyc_verified = serializer.validated_data.get(
+                "kyc_verified", instance.kyc_verified
+            )
 
             if kyc_verified and not kyc_submitted:
                 return Response(
-                    {
-                        "error": "KYC must be submitted before verification."
-                    },
-                    status=status.HTTP_400_BAD_REQUEST
+                    {"error": "KYC must be submitted before verification."},
+                    status=status.HTTP_400_BAD_REQUEST,
                 )
 
             instance.kyc_submitted = kyc_submitted
@@ -48,7 +55,9 @@ class AccountVerificationView(generics.UpdateAPIView):
 
             if kyc_submitted and kyc_verified:
                 instance.kyc_verified = kyc_verified
-                instance.verification_date = serializer.validated_data.get("verification_date", timezone.now())
+                instance.verification_date = serializer.validated_data.get(
+                    "verification_date", timezone.now()
+                )
                 instance.verification_notes = serializer.validated_data.get(
                     "verification_notes", ""
                 )
@@ -62,8 +71,64 @@ class AccountVerificationView(generics.UpdateAPIView):
             return Response(
                 {
                     "message": "Account verification status updated successfully",
-                    "data": self.get_serializer(instance).data
+                    "data": self.get_serializer(instance).data,
                 },
-                status=status.HTTP_200_OK
+                status=status.HTTP_200_OK,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DepositView(generics.CreateAPIView):
+    serializer_class = DepositSerializer
+    renderer_classes = [GenericJSONRenderer]
+    object_label = "deposit"
+    permission_classes = [IsTeller]
+
+    def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        account_number = request.query_params.get("account_number")
+        if not account_number:
+            return Response(
+                {"error": "Account number must be provided before a deposit can be made."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            account = BankAccount.objects.get(account_number=account_number)
+            serializer = CustomerInfoSerializer(account_number)
+            return Response(serializer.data)
+        except BankAccount.DoesNotExist:
+            return Response(
+                {"error": "Account number does not exist."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        
+    @transaction.atomic
+    def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        account = serializer.context["account"]
+        amount = serializer.validated_data.get("amount")
+
+        try:
+            account.account_balance += amount
+            account.full_clean()
+            account.save()
+            logger.info(f"Deposit of {amount} made to {account.account_number} by teller {request.user.email} was successfully")
+
+            send_deposit_email(account.user, account.user.email, amount, account.currency, account.account_balance, account.account_number)
+            return Response(
+                {
+                    "message": f"Successfully deposited {amount} to account {account.account_number}",
+                    "new_balanc" : str(account.account_balance)
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        except Exception as e:
+            logger.exception(f"An error occured during deposit: {str(e)}")
+            return Response(
+                {"error": "An error occured during deposit"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
